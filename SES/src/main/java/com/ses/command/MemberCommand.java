26694fafb353fff92141f2588a3f7268fba0aeaf
DanMooG <tytyjacob5514@gmail.com>
2020-06-02 14:32:58 +0900

    다시 DTO, DAO, Command 작성

---
+++ SES/src/main/java/com/ses/command/MemberCommand.java
@@ -0,0 +1,5 @@
+package com.ses.command;
+
+public class MemberCommand {
+
+}
---
+++ SES/src/main/java/com/ses/dao/SignUpDAO.java
@@ -0,0 +1,32 @@
+package com.ses.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.activation.DataSource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class SignUpDAO {
+    Connection con = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+    
+    private DataSource dataSource;
+    
+    public SignUpDAO() {
+        try {
+            Context context = new InitialContext();
+            dataSource = (DataSource)context.lookup("java:comp/env/jdbc/Oracle11g");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static SignUpDAO instance = new SignUpDAO();
+    
+    public static SignUpDAO getInstance() {
+        return instance;
+    }
+}
