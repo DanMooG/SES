d3acf55afb0eef8fa92c1e355bc11207f8fa4fb2
maneca <sung261817@gmail.com>
2020-06-07 17:59:37 +0900

    재정비

--- SES/src/main/java/com/ses/controller/MemberController.java
+++ SES/src/main/java/com/ses/controller/MemberController.java
@@ -1,5 +1,58 @@
 package com.ses.controller;
 
-public class MemberController {
+import java.io.PrintWriter;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.ses.dto.MemberDTO;
+import com.ses.service.MemberService;
+
+public class MemberController {
+   @Autowired
+   MemberService mService;
+
+   @Inject
+   HttpSession session;
+   
+   // 로그인
+   @RequestMapping(value = "/doLogin", method = RequestMethod.POST)
+   public String MLogin(HttpServletResponse response, HttpServletRequest request, RedirectAttributes attr)
+         throws Exception {
+      System.out.println("Controller - Login()");
+      MemberDTO dto = mService.MLogin(request);
+      
+      if (dto == null) { // 해당 회원이 존재하지 않을 경우
+         session.setAttribute("mId", null);
+         session.setAttribute("mPw", null);
+         response.setContentType("text/html; charset=UTF-8");
+         PrintWriter out = response.getWriter();
+         out.println("<script>alert('존재하지 않는 회원입니다!!'); document.location.href='login';</script>");
+         out.flush();
+      } else if (!dto.getM_PW().equals(request.getParameter("M_PW"))) { // 비밀번호가 일치하지 않을 경우
+         session.setAttribute("mId", null);
+         session.setAttribute("mPw", null);
+         response.setContentType("text/html; charset=UTF-8");
+         PrintWriter out = response.getWriter();
+         out.println("<script>alert('비밀번호가 틀렸습니다!!'); document.location.href='login';</script>");
+         out.flush();
+      } else { // 성공
+         session.setAttribute("mId", dto.getM_ID());
+         session.setAttribute("mPw", dto.getM_PW());
+         response.setContentType("text/html; charset=UTF-8");
+         PrintWriter out = response.getWriter();
+         out.println("<script>alert('환영합니다!!'); document.location.href='main'</script>");
+         out.flush();
+         return "list";
+      }
+
+      return "main";
+   }
 }
